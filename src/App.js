5814c482c158156b2dfad967294ee478a074d419
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-07 16:45:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header >
-       <Widget name="Service providers List-RCX-58-a"/>
+       <Widget name="Service providers List - RCX-62"/>
       </header>
       
     </div>
