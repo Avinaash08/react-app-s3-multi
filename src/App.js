d8f5d96a8e205218a4af3ff663f7ac449e3cac1a
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-07 16:48:54 +0530

    Multi commit one

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header >
-       <Widget name="Service providers List - RCX-62"/>
+       <Widget name="Service providers List - RCX-62 - Multi commit 1"/>
       </header>
       
     </div>
