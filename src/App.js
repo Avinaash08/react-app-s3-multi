45c6eea2538298171f325b98d78f2b836b068ea8
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-13 16:17:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header >
-       <Widget name="Service providers List - RCXN-16"/>
+       <Widget name="Service providers List - RCXN-16 Multi commit"/>
       </header>
       
     </div>
