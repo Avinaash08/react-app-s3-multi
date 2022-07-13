d3f3b6df9b8df767ca0a8b64b0fcab5b99c02435
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-13 16:20:15 +0530

    second

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header >
-       <Widget name="Service providers List - RCXN-16 Multi commit"/>
+       <Widget name="Service providers List - RCXN-16 Multi commit second"/>
       </header>
       
     </div>
